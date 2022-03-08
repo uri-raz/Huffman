0dc8fc302aa4845d89e0c46949f2a0eeeafa7eea
Uri Raz <uri.raz@gmail.com>
2022-03-08 20:20:53 +0200

    Improve file error handling

--- Huffman.cpp
+++ Huffman.cpp
@@ -4,19 +4,38 @@
 
 int main()
 {
+	try {
+		Huffman<char>::compressed huf;
+		huf.load("D:\\Uri Raz\\nonexistent.txt");
+	}
+	catch (std::filesystem::filesystem_error) {
+		std::cout << "OK: File not found\n";
+	}
+
+	try {
+		Huffman<char>::compressed huf;
+		huf.load("D:\\Uri Raz\\empty.txt");
+	}
+	catch (std::filesystem::filesystem_error) {
+		std::cout << "OK: Corrupt file\n";
+	}
+
+	std::cout << '\n';
+
 	std::string data;
 
-	FILE* inFile = fopen("...\\File.txt", "r");
+	FILE* inFile = fopen("D:\\Uri Raz\\Equipment.txt", "r");
+//	FILE* inFile = fopen("D:\\Uri Raz\\C++ Projects\\Huffman\\Tooth filtered from 116 photos.txt", "r");
 	int c;
 	while ((c = std::fgetc(inFile)) != EOF)
 		data += c;
 	fclose(inFile);
 
-	Huffman huf;
+	Huffman<char> huf;
 	auto compressed = huf.compress(data);
-	compressed.save("...\\compressed.huf");
-	Huffman::compressed chuchu;
-	chuchu.load("...\\compressed.huf");
+//	compressed.save("D:\\Uri Raz\\C++ Projects\\Huffman\\compressed.huf");
+//	Huffman<char>::compressed chuchu;
+//	chuchu.load("D:\\Uri Raz\\C++ Projects\\Huffman\\compressed.huf");
 	auto str = huf.decompress(compressed);
 	std::cout << str;
 }
